aee07e2f808542ec46c3858e56b71f827cba0ace
Benjamin Bergstein <bennyjbergstein+github@gmail.com>
2020-05-08 18:58:06 +0000

    Update homepage screenshot.

--- app/utils/infoSections.tsx
+++ app/utils/infoSections.tsx
@@ -2,22 +2,22 @@ export default [
   {
     headline: 'Get to know your starter\'s habits',
     body: 'Prideful, open-crumb bread starts with a super active starter. Learn & make adjustments to mix your dough at peak activity. Every starter culture has a unique routine.',
-    backgroundPosition: 0,
+    backgroundPosition: -180,
     first: true,
   },
   {
     headline: 'Fine-tune dough strengthening techniques',
     body: 'Sourdough breads are often made with a high proportion of water to flour. High-hydration doughs require careful care to develop gluten strength. Experiment with different techniques and timings for stronger dough, and better bakes.',
-    backgroundPosition: -470,
+    backgroundPosition: -475,
   },
   {
     headline: 'Avoid over-proofing.',
     body: 'Levain has a finite maturation period before leavening power is exhausted. After that, the final product will rise less and taste too acidic. Avoid a flat, sour result by knowing the typical lifetime of your levain.',
-    backgroundPosition: -230,
+    backgroundPosition: -325,
   },
   {
     headline: 'Incorporate new techniques',
     body: 'Basic sourdough recipes often forego advanced techniques. Autolyse is a method for starting gluten development early, by combining flour and water adding the levain. When flour and water make contact, gluten development begins. This technique, which can last for hours before mixing, supercharges dough strength without over-proofing.',
-    backgroundPosition: -115,
+    backgroundPosition: -235,
   },
 ]
